7731754894c92539aa28b07028b88fedaf37e699
kevini78 <133727543+kevini78@users.noreply.github.com>
2023-08-16 22:37:52 -0300

    Add files via upload

---
+++ tarefa4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+  int num1;
+  int dolar;
+  int resultadoDivisao;
+  printf("Qual é o valor a ser convertido  ");
+  scanf("%d",&num1);
+  printf("Qual o valor atual do dolar ");
+  scanf("%d",&dolar);
+  resultadoDivisao = dolar / num1;
+  printf("O valor em dolar é %d",resultadoDivisao);
+  
+  
+  
+  
+}
